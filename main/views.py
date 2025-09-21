0f7030ca04d82ec3a55b1f24bd47a9bd1d31ba1d
RafasyaSubhan <rafasya.subhan@gmail.com>
2025-09-21 14:13:06 +0700

    Code untuk Tugas 4

--- main/views.py
+++ main/views.py
@@ -1,17 +1,31 @@
+import datetime
+from django.urls import reverse
 from django.shortcuts import render, redirect, get_object_or_404
 from main.forms import ProductForm
 from main.models import Product
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib import messages
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login')
 def show_main(request):
-    products_list = Product.objects.all()
+    filter_type = request.GET.get("filter", "all")
+
+    if filter_type == "all":
+        products_list = Product.objects.all()
+    else:
+        products_list = Product.objects.filter(user=request.user)
+        
     context = {
         'npm' : '2406409542',
         'name': 'Rafasya Muhammad Subhan',
         'class': 'PBP A',
-        'products_list' : products_list
+        'products_list' : products_list,
+        'last_login': request.COOKIES.get('last_login', 'Never')
     }
 
     return render(request, "main.html", context)
@@ -20,12 +34,15 @@ def add_product(request):
     form = ProductForm(request.POST or None)
 
     if form.is_valid() and request.method == "POST":
-        form.save()
+        products_entry = form.save(commit=False)
+        products_entry.user = request.user
+        products_entry.save()
         return redirect('main:show_main')
 
     context = {'form': form}
     return render(request, "add_product.html", context)
 
+@login_required(login_url='/login')
 def show_product(request,id):
     products = get_object_or_404(Product, pk=id)
     products.add_visitor()
@@ -66,3 +83,37 @@ def show_json_by_id(request, product_id):
         return HttpResponse(json_data, content_type="application/json")
     except Product.DoesNotExist:
         return HttpResponse(status=404)
+    
+def register(request):
+    form = UserCreationForm()
+
+    if request.method == "POST":
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Your account has been successfully created!')
+            return redirect('main:login')
+    context = {'form':form}
+    return render(request, 'register.html', context)
+
+def login_user(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            response = HttpResponseRedirect(reverse("main:show_main"))
+            response.set_cookie('last_login', str(datetime.datetime.now()))
+            return response
+
+    else:
+        form = AuthenticationForm(request)
+    context = {'form': form}
+    return render(request, 'login.html', context)
+
+def logout_user(request):
+    logout(request)
+    response = HttpResponseRedirect(reverse('main:login'))
+    response.delete_cookie('last_login')
+    return response
